9d32c29fad3e71ec0eee5f84fde6c3337b1e9608
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2024-04-03 13:10:19 +0400

    finalized validation logic

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true, // 👈
+      whitelist: true,
+    }),
+  );
 
   await app.listen(3000);
 }
