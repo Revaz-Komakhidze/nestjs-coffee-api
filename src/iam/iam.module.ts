b446ca01b8775813ba1e66337979ab04e72b32c6
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2024-04-18 12:30:18 +0400

    updated iam module

---
+++ src/iam/iam.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+import { BcryptService } from './hashing/bcrypt.service';
+import { HashingService } from './hashing/hashing.service';
+import { AuthenticationController } from './authentication/authentication.controller';
+import { AuthenticationService } from './authentication/authentication.service';
+import { JwtModule } from '@nestjs/jwt';
+import jwtConfig from './config/jwt.config';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from './authentication/guards/access-token.guard';
+import { RolesGuard } from './authorization/guards/roles.guard';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { RefreshTokenIdsStorage } from './authentication/refresh-token-ids.storage';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    ConfigModule.forFeature(jwtConfig),
+    TypeOrmModule.forFeature([User]),
+  ],
+  providers: [
+    {
+      provide: HashingService,
+      useClass: BcryptService,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+    AuthenticationService,
+    RefreshTokenIdsStorage,
+  ],
+  controllers: [AuthenticationController],
+})
+export class IamModule {}
