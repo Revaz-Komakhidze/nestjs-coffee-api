2e6a04d0a11ebcad23925ac69d937dae3416991a
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2024-04-23 13:12:59 +0400

    updated coffees module and added unit tests for coffees service

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -9,17 +9,20 @@ import {
   Query,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
+import { Public } from 'src/common/decorators/public.decorator';
+import { Roles } from 'src/iam/authorization/decorators/roles.decorator';
+import { Role } from 'src/users/enums/role.enum';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
+  @Public()
   @Get()
   findAll(@Query() paginationQuery) {
     const { limit, offset } = paginationQuery;
-    offset;
-    limit;
-    return this.coffeesService.findAll();
+
+    return this.coffeesService.findAll({ limit, offset });
   }
 
   @Get(':id')
@@ -27,16 +30,19 @@ export class CoffeesController {
     return this.coffeesService.findOne(id);
   }
 
+  @Roles(Role.Admin)
   @Post()
   create(@Body() body) {
     return this.coffeesService.create(body);
   }
 
+  @Roles(Role.Admin)
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
     return this.coffeesService.update(id, body);
   }
 
+  @Roles(Role.Admin)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.coffeesService.remove(id);
